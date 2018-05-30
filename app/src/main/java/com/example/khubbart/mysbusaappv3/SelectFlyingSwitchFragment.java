b320957e501787ef4f175c33cecb0ade44fa51ae
hublaw21 <kevin@hubbart.com>
2018-05-30 14:32:22 -0400

    Jump and Spin Lookup Working Better

---
+++ app/src/main/java/com/example/khubbart/mysbusaappv3/SelectFlyingSwitchFragment.java
@@ -0,0 +1,68 @@
+package com.example.khubbart.mysbusaappv3;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+//import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ToggleButton;
+
+
+public class SelectFlyingSwitchFragment extends Fragment {
+
+    private OnChangeFlyingSwitchInteractionListener mListener;
+    public ToggleButton flyingButton;
+
+    public SelectFlyingSwitchFragment(){}
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+            View view = inflater.inflate(R.layout.fragment_select_flying_switch,
+                    container, false);
+
+            flyingButton = view.findViewById(R.id.toggleButtonFlyingEntry);
+
+            flyingButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isFlying) {
+                    onFlyingButtonChanged(isFlying);
+                }
+            });
+
+            return view;
+    }
+
+    public void onFlyingButtonChanged(boolean isFlying){
+        if(mListener != null){
+            mListener.onChangeFlyingSwitchInteraction(isFlying);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context instanceof OnChangeFlyingSwitchInteractionListener) {
+            mListener = (OnChangeFlyingSwitchInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + "must implement OnChangeFlyingSwitchInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnChangeFlyingSwitchInteractionListener{
+        public void onChangeFlyingSwitchInteraction(boolean isFlying);
+    }
+
+}
