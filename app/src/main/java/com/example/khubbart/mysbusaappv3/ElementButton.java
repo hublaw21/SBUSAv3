128d9eb9f44e5f0655896d6519a22ec232277cb8
hublaw21 <kevin@hubbart.com>
2018-09-06 19:51:54 -0400

    Scoring 2.91

--- app/src/main/java/com/example/khubbart/mysbusaappv3/ElementButton.java
+++ app/src/main/java/com/example/khubbart/mysbusaappv3/ElementButton.java
@@ -1,8 +1,12 @@
 package com.example.khubbart.mysbusaappv3;
 
 import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.AppCompatButton;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ElementButton extends android.support.v7.widget.AppCompatButton {
 
@@ -10,5 +14,14 @@ public class ElementButton extends android.support.v7.widget.AppCompatButton {
 
     public ElementButton(Context context) {
         super(context);
+        /*
+        this.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeButtonDialog(0);
+            }
+        });
+        */
     }
+
 }
